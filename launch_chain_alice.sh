15b6812c95bdc8ac96043a6fef07ea9e8f665b33
Koichi Matsuda <k-matsuda@caldia.org>
2019-04-09 17:31:39 +0900

    add launch scripts

---
+++ launch_chain_alice.sh
@@ -0,0 +1,11 @@
+cargo run --release \-- \
+  --base-path /tmp/alice \
+  --chain=local \
+  --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
+  --telemetry-url ws://telemetry.polkadot.io:1024 \
+  --alice \
+  --validator
+  
+# cargo run --release \-- \
+#   purge-chain \
+#   --base-path /tmp/alice \
---
+++ launch_chain_bob.sh
@@ -0,0 +1,12 @@
+cargo run --release \-- \
+  --base-path /tmp/bob \
+  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/QmQZ8TjTqeDj3ciwr93EJ95hxfDsb9pEYDizUAbWpigtQN \
+  --chain=local \
+  --bob \
+  --port 30334 \
+  --telemetry-url ws://telemetry.polkadot.io:1024 \
+  --validator
+  
+# cargo run --release \-- \
+#   purge-chain \
+#   --base-path /tmp/bob \
